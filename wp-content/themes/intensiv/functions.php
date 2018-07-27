4da8233f4b5c51ddd04b732f6179710df8617fc2
Eduard Malozhylenko <malozhylenko@gmail.com>
2018-07-27 18:08:46 +0300

    post_type

--- wp-content/themes/intensiv/functions.php
+++ wp-content/themes/intensiv/functions.php
@@ -164,9 +164,16 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Register Post Type.
+ */
+require get_template_directory() . '/inc/post-type.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+
 
